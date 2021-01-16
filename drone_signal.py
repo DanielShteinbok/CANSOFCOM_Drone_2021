55eb686d18659cc13136ccb0dd30b5224c93885c
Joshua Weland <weland.joshua@hotmail.com>
2021-01-15 19:13:07 -0500

    Added Noisy Signal Function

--- drone_signal.py
+++ drone_signal.py
@@ -1,33 +1,10 @@
-#import math
+import math
 import numpy
 
 def sinc(x) :
     if x == 0: 
         return 1
     return math.sin(x)/x
-
-# DEPRECATED
-#def return_signal(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, t) :
-    #'''
-    #returns the amplitude of the radio waves received from a drone,
-    #according to the provided equation at a given time t
-    #'''
-    ## sigma is the variable that holds the result of the summation
-    #sigma = 0
-    #for n in range(N) :
-        #sigma += math.e**( \
-            #-1j * 4 * math.pi / lam * \
-            #((L1+L2)/2) * \
-                #math.cos(theta) * \
-                #math.sin(2 * math.pi * frot * t + (2*math.pi*n)/N) 
-        #) * sinc( \
-            #(math.pi*4/lam) * \
-            #((L2-L1)/2) * \
-            #math.cos(theta) * \
-            #math.sin( ( (2*math.pi) * (frot * t + n/N) ) ) \
-        #)
-       #
-    #return Ar*math.e**(1j*(2*math.pi*fc*t - ( ( (4*math.pi)/lam) * (R+Vrad*t) )  ) ) * sigma
 
 def return_signal_array(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, t):
     ''' (num, num, num, num, num, num, num, num, num, num, numpy.ndarray) -> numpy.ndarray
@@ -72,42 +49,41 @@ def return_signal_array(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, t):
        
     return Ar*numpy.exp(1j*(2*numpy.pi*fc*t - 4*numpy.pi/lam * (R + Vrad * t)) ) * sigma   
 
+def return_noisey_signal_array(inputSignal, SNR, Ar ) :
+    """ (ndarray, int, int) --> ndarray 
+        SNR(signal to noise ratio) in dB
+        Ar or ScaleFactor from equation
+    """  
+    sigma = math.sqrt(Ar**2/(10**(SNR/10)))
+    return inputSignal + numpy.random.normal(0, sigma, len(inputSignal))
+
 # SCRIPT FOR TESTING THE ABOVE
 
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # example values to use
-#Ar = 1
-#L1 = 1
-#L2 = 0
-#N = 4
-#R = 0
-#Vrad = 1
+# Ar = 1
+# L1 = 1
+# L2 = 0
+# N = 4
+# R = 0
+# Vrad = 1
 
-#theta = numpy.pi/2 #depression angle say 30deg
-#fc = 60e9 #Frequency, open question to find best frequency
-#lam = 299792458/fc #wavelength wavelength = c / frequency
-#fs = 1 #sampling frequency
-#frot = 500 #frequency of drone prop rotation
+# theta = numpy.pi/2 #depression angle say 30deg
+# fc = 60e9 #Frequency, open question to find best frequency
+# lam = 299792458/fc #wavelength wavelength = c / frequency
+# fs = 1 #sampling frequency
+# frot = 500 #frequency of drone prop rotation
 
-#------------------------------------------------------------
-# testing the deprecated function:
-
-#time = []
-#output = []
-
-#for i in range(100):
-    #time.append(i)
-    #output.append(return_signal(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, fs*i))
-
-#plt.plot(time, output)
-#plt.show()
-
-#---------------------------------------------------------
 # testing the new numpy-using function:
 
-#time_array = numpy.arange(100)
-#plt.plot(time_array, return_signal_array(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, time_array))
-#plt.show()
+
+# time_array = numpy.arange(100)
+# signal_array = return_signal_array(Ar, L1, L2, N, R, Vrad, lam, theta, fc, frot, time_array)
+# noisySignal = return_noisey_signal_array(signal_array, 10, Ar)
+
+
+# plt.plot(time_array, noisySignal)
+# plt.show()
 
 
